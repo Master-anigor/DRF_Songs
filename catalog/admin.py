87d688c24aa46ac18d1035e97c4e2f10d250bc02
Master-anigor <soldatov056@yandex.ru>
2022-11-03 21:22:17 +0500

    Update admin.py
    add all model

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,3 +3,6 @@ from .models import MusicianPerformer, Album, Song, SongInAlbum
 
 
 admin.site.register(MusicianPerformer)
+admin.site.register(Album)
+admin.site.register(Song)
+admin.site.register(SongInAlbum)
